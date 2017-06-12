6c8323639fac3523774f1eab8186cca37bdf4e2c
Rodion Malinovsky <rodionmalino@gmail.com>
2017-06-12 07:08:58 +0300

    Add stub for engine test with test plan

---
+++ test/core/engine_test.cc
@@ -0,0 +1,13 @@
+// Copyright [2016] <Malinovsky Rodion>
+
+#include "core/engine.h"
+#include "gtest/gtest.h"
+
+// Implement Engine test
+// 1. Create a fixture, take base code from EngineLauncher
+// 2. create cond var, which will emulate  SIGINT
+// 3. run engine -> create server socket
+// 4. create tcp client and write data to server socket
+// 5. verify output of the echo server.
+
+#error missing-implementation
